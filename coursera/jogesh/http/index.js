c3a4fe08b6ef29efc93afae545e7b093d8e82651
whodanyalahmed <daniahmedkhatri@gmail.com>
2020-08-23 17:05:36 +0500

    first commit

---
+++ coursera/jogesh/http/index.js
@@ -0,0 +1,53 @@
+const http = require('http');
+const fs = require('fs');
+const path = require('path')
+
+const hostname = "localhost";
+const port = 3000;
+
+const server = http.createServer((req,res) => {
+    console.log('Request for ' + req.url + " by method " + req.method);
+    if(req.method == 'GET'){
+        var fileURl;
+        if(req.url == '/') fileURl = '/index.html';
+        else fileURl = req.url;
+
+        var filePath = path.resolve('./public'+ fileURl);
+        const fileExt = path.extname(filePath);
+        if(fileExt == '.html'){
+            fs.exists(filePath , (exists) => {
+                if(!exists) {
+                    res.statusCode = 404;
+                    res.setHeader('Content-Type','text/html');
+                    res.end('<html><body><h1>Error 404: ' + fileURl+ ' not found</h1></body></html>');
+                    return;
+                }
+                res.statusCode = 200;
+                res.setHeader('Content-Type','text/html');
+                fs.createReadStream(filePath).pipe(res);
+            })
+        }
+        else{
+            res.statusCode = 404;
+            res.setHeader('Content-Type','text/html');
+            res.end('<html><body><h1>Error 404: ' + fileURl+ ' not an html</h1></body></html>');
+            return;
+        }
+
+    }
+    else{
+        res.statusCode = 404;
+        res.setHeader('Content-Type','text/html');
+        res.end('<html><body><h1>Error 404: ' + fileURl+ ' not supported`</h1></body></html>');
+        return;
+
+    }
+
+    // res.statusCode = 200;
+    // res.setHeader('Content-Type' , 'text/html');
+})
+
+
+server.listen(port,hostname,() => {
+    console.log(`Server running at http://${hostname}:${port}`);
+})
