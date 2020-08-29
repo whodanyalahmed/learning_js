33dae370210a2c6dd952051b0d2d8044a17aef36
whodanyalahmed <daniahmedkhatri@gmail.com>
2020-08-29 13:01:30 +0500

    first commit

--- coursera/jogesh/express/index.js
+++ coursera/jogesh/express/index.js
@@ -1,22 +1,62 @@
 const express = require('express')
 const http = require('http');
 const bodyPraser =require('body-parser')
-// const morgan = require('morgan');
+const morgan = require('morgan');
 
 const hostname = 'localhost';
 const port = 3000;
 
 const app = express();
-// app.use(morgan('dev'));
+app.use(morgan('dev'));
 app.use(bodyPraser.json());
 
-app.all("/dishes",(res,req,next) => {
+app.all('/dishes',(req,res,next) => {
     res.statusCode = 200;
-    res.setHeader('Content-Type','text/plain' );
+    res.setHeader('Content-Type','text/plain');
     next();
 });
 
-// app.use(express.static(__dirname + '/public'));
+
+app.get('/dishes',(req,res,next) => {
+    res.end("will send all the dishes to you.... from database")
+});
+
+app.post('/dishes',(req,res,next) => {
+    res.end("Will add the dishes: " + req.body.name + 
+             ' with details: ' + req.body.description);
+});
+app.put('/dishes',(req,res,next) => {
+    res.statusCode = 403;
+    res.end("PUT operation not supported...");
+});
+
+
+app.delete('/dishes',(req,res,next) => {
+    res.end("Deleting all the dishes.... from database")
+});
+
+
+
+app.get('/dishes/:dishID',(req,res,next) => {
+    res.end("will send details of the dishe.... "+req.params.dishID + ' to you.')
+});
+
+app.post('/dishes/:dishID',(req,res,next) => {
+    res.statusCode = 403;
+    res.end("POST operation not supported... /dishes/" + req.params.dishID);
+});
+app.put('/dishes/:dishID',(req,res,next) => {
+    res.write("updating the dish " + req.params.dishID);
+    res.end("This will update the dish: " + res.body.name + ' with details: ' + req.body.description);
+});
+
+
+app.delete('/dishes/:dishID',(req,res,next) => {
+    res.end("Deleting dish: ...." + req.params.dishID);
+});
+
+
+app.use(express.static(__dirname + '/public'));
 
 app.use((req,res,next) => {
     console.log(req.headers);
